45f6b7f7a372270d0559b7aa76f9e838ab84c887
Kshitij <kshitijmittal2007@gmail.com>
2026-02-07 12:04:29 +0530

    Update Experiment_3.sql

--- Experiment_3.sql
+++ Experiment_3.sql
@@ -12,8 +12,7 @@ SELECT ename FROM employee WHERE ename LIKE 'S%';
 -- 4. Display the names of employees whose name ends with alphabet S. 
 SELECT ename FROM employee WHERE ename LIKE '%S';
 
--- 5. Display the names of employees working in department number 10 or 20 or 40 or employees working as clerks, salesman or 
--- analyst. 
+-- 5. Display the names of employees working in department number 10 or 20 or 40 or employees working as clerks, salesman or analyst. 
 SELECT ename FROM employee WHERE deptno IN (10,20,40) AND job IN ('CLERK', 'SALESMAN', 'ANALYST');
 
 -- 6. Display employee number and names for employees who earn commission. 
@@ -23,7 +22,10 @@ SELECT ename, empno FROM employee WHERE comm IS NOT NULL AND comm > 0;
 SELECT empno, (sal + IFNULL(comm, 0)) AS total_salary FROM employee;
 
 -- 8. Display employee number and annual salary for each employee. 
-select empno, (sal)*12 as annual_sal;
+SELECT empno, (sal + IFNULL(comm, 0))*12 AS ANNUAL_SALARY FROM employee;
 
 -- 9. Display the names of all employees working as clerks and drawing a salary more than 3,000. 
+SELECT ename FROM employee WHERE job = 'CLERK' AND SAL > 3000;
+
 -- 10. Display the names of employees who are working as clerk, salesman or analyst and drawing a salary more than 3,000. 
+SELECT ename FROM employee WHERE job IN ('CLERK', 'SALESMAN', 'ANALYST') AND sal > 3000;
